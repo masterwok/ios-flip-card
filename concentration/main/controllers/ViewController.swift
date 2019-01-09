e486a45cd8c9eec82f6ab48cebb65078521e7a6b
Jonathan Trowbridge <jonathantrowbridge@Jonathans-MacBook-Pro.local>
2019-01-09 16:32:06 -0500

    master - Addding Cards Through Interface Builder
    Removed programattic constraints in favor of interface builder built controls. The downside of this is that the number of cards can't be dynamic.

--- concentration/main/controllers/ViewController.swift
+++ concentration/main/controllers/ViewController.swift
@@ -10,31 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cardButtons: [UIButton]!
+    
     private let concentration = Concentration(
             emojis: ["👻", "💀", "☠️", "👽", "👾", "🤖"]
     )
 
+    private var cardContainerView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-//        let customView = CardView(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
-
-        let margins = view.layoutMarginsGuide
-        let containerView = UIView()
-
-        containerView.backgroundColor = UIColor.red
-
-        view.addSubview(containerView)
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-
-        view.addConstraints([
-            NSLayoutConstraint(item: containerView, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .width, multiplier: 1, constant: 40)
-            , NSLayoutConstraint(item: containerView, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .height, multiplier: 1, constant: 40)
-            , NSLayoutConstraint(item: containerView, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
-            , NSLayoutConstraint(item: containerView, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)
-        ])
-
-
     }
 
 }
